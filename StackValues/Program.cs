fa52c8020b71fecc9b8662bd55177158a5c988e6
beerappa <beerappadandaragi@gmail.com>
2021-11-18 12:18:56 +0530

    Data Structuring Program in c# generics

---
+++ StackValues/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+            Console.WriteLine("Data Structure Problems using  C# Generics");
+        Console.ReadLine();
+
+    }
+  
+}
