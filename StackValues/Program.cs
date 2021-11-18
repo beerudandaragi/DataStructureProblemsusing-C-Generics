30bdf514652ebdb4125412a3403fb6adf1214b32
beerappa <beerappadandaragi@gmail.com>
2021-11-18 12:40:40 +0530

    Data Structuring Program in c# generics

--- StackValues/Program.cs
+++ StackValues/Program.cs
@@ -1,38 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Program
+public class QueueExample
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        Stack<int> StackObjects = new Stack<int>();
-        StackObjects.Push(56);
-        StackObjects.Push(30);
-        StackObjects.Push(70);
-
-
-        foreach (int item in StackObjects)
+        Queue<int> names = new Queue<int>();
+        names.Enqueue(56);
+        names.Enqueue(30);
+        names.Enqueue(70);
+        
+        foreach (int name in names)
         {
-            Console.WriteLine("Ouptut of storedvalues:" + item);
+            Console.WriteLine(name);
         }
-        {
-            Console.WriteLine("Peak element:" + StackObjects.Peek());
-            Console.WriteLine("pop of the storedvalues:" + StackObjects.Pop());
-            Console.WriteLine("after the peek and pop  stored values:" + StackObjects.Count);
-        }
-        {
-            Console.WriteLine("Peak element:" + StackObjects.Peek());
-            Console.WriteLine("pop of the storedvalues:" + StackObjects.Pop());
-            Console.WriteLine("after the peek and pop  stored values:" + StackObjects.Count);
-        }
-        {
-            Console.WriteLine("Peak element:" + StackObjects.Peek());
-            Console.WriteLine("pop of the storedvalues:" + StackObjects.Pop());
-            Console.WriteLine("after the peek and pop  stored values:" + StackObjects.Count);
-
-        }
-        Console.ReadLine();
-
-    }
-
-}
+        Console.ReadKey();
+    }      }  
