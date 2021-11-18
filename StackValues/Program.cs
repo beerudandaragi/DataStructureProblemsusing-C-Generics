205e87866238e158541a1e5491a762dd1f9bd06f
beerappa <beerappadandaragi@gmail.com>
2021-11-18 14:17:56 +0530

    Data Structuring Program in c# generics

--- StackValues/Program.cs
+++ StackValues/Program.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class QueueExample
+namespace StackQueue_uc4
 {
-    public static void Main(string[] args)
+    class Program
     {
-        Queue<int> names = new Queue<int>();
-        names.Enqueue(56);
-        names.Enqueue(30);
-        names.Enqueue(70);
-        
-        foreach (int name in names)
+        static void Main(string[] args)
         {
-            Console.WriteLine(name);
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------");
+            queue.Dequeue();
+
+            foreach (int item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine(queue.Peek());
+            Console.ReadLine();
         }
-        Console.ReadKey();
-    }      }  
+        
+    }
+}
